32d24862c587c97ae2db9b0a97ce7729e717dbe2
amandesai01 <amandesai01@gmail.com>
2023-08-16 00:04:59 +0530

    scaffold project

---
+++ app.config.ts
@@ -0,0 +1,11 @@
+export default defineAppConfig({
+  site: {
+    name: "Tech Laundry",
+  },
+  dataSource: {
+    ghAdminUsername: "amandesai01",
+    ghRepoUsername: "amandesai01",
+    ghRepoName: "techlaundry",
+    ghDsIssueNumber: 1,
+  },
+});
---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  extends: [
+    'github:unproducts/magzyne/packages/web-layer'
+  ],
+})
